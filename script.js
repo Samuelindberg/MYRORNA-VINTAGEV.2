6ea5f99dc665dec03169fc96c77c49f03ac90071
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-24 12:25:49 +0100

    vit backgrund, nya färger och ny navbaridé

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let navitems = document.querySelectorAll(".nav-items");
-console.log(navitems);
-
-navitems.forEach(() => {});
+// const nav = document.getElementById("nav-bar");
+// document.getElementById("logo");
+// window.addEventListener("scroll", () => {
+//   console.log("test");
+//   console.log(window.scrollY);
+//   if (window.scrollY > 35) {
+//     nav.style.position = "fixed";
+//   } else {
+//     nav.style.position = "relative";
+//   }
+// });
